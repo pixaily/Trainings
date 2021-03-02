93a21f29d791df2313ea300d4f54a9a84fb197aa
Deepaw <danield.georgiev@gmail.com>
2021-03-02 16:41:17 +0200

    Adopt me - Pure React - Passing in Component Props

--- javascript/react/adopt-me/src/App.js
+++ javascript/react/adopt-me/src/App.js
@@ -1,17 +1,17 @@
-const Pet = () => {
+const Pet = (props) => {
   return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Ares"),
-    React.createElement("h2", {}, "Dog"),
-    React.createElement("h3", {}, "Labrador"),
+    React.createElement("h1", {}, props.name),
+    React.createElement("h2", {}, props.animal),
+    React.createElement("h3", {}, props.breed),
   ]);
 };
 
 const App = () => {
   return React.createElement("div", { id: "something-important" }, [
     React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet),
-    React.createElement(Pet),
-    React.createElement(Pet),
+    React.createElement(Pet, { name: "Ares", animal: "dog", breed: "Labrador", }),
+    React.createElement(Pet, { name: "Max", animal: "dog", breed: "Shepard" }),
+    React.createElement(Pet, { name: "Rico", animal: "cat", breed: "Grey" }),
   ]);
 };
 
