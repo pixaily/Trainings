59a585f607b1a74670863c851e03c9e600b0fd5e
Deepaw <danield.georgiev@gmail.com>
2021-03-03 23:59:18 +0200

    Adopt Me - Hooks - Creating a Search Component

--- javascript/react/adopt-me/src/App.js
+++ javascript/react/adopt-me/src/App.js
@@ -1,14 +1,12 @@
 import React from "react";
 import { render } from "react-dom";
-import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
       <h1 id="something-important">Adopt Me!</h1>
-      <Pet name="Ares" animal="dog" breed="Labrador" />
-      <Pet name="Max" animal="dog" breed="Shepard" />
-      <Pet name="Rico" animal="dog" breed="Grey" />
+      <SearchParams />
     </div>
   );
 };
---
+++ javascript/react/adopt-me/src/SearchParams.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const SearchParams = () => {
+  const location = "Seattle, WA";
+
+  return (
+    <div className="search-params">
+      <form>
+        <label htmlFor="location">
+          Location
+          <input
+            type="text"
+            id="location"
+            value={location}
+            placeholder="Location"
+          />
+        </label>
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default SearchParams;
