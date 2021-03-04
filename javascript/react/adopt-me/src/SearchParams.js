ac652d17f01f9b2035d6962e6813e03203fafd9a
Deepaw <danield.georgiev@gmail.com>
2021-03-05 00:32:50 +0200

    Adopt Me - Effects - Declaring Effect Dependencies

--- javascript/react/adopt-me/src/SearchParams.js
+++ javascript/react/adopt-me/src/SearchParams.js
@@ -6,7 +6,7 @@ const SearchParams = () => {
   const [location, setLocation] = useState("Seattle, WA");
   const [breeds, setBreeds] = useState([]);
   const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
-  const [breed, BreedDropdown] = useDropdown("Breed", "", breeds);
+  const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds);
 
   useEffect(() => {
     setBreeds([]);
@@ -16,7 +16,7 @@ const SearchParams = () => {
       const breedStrings = breeds.map(({ name }) => name);
       setBreeds(breedStrings);
     }, console.error);
-  });
+  }, [animal, setBreeds, setBreed]);
 
   return (
     <div className="search-params">
