673a7a75f2ec8b437fe7d7f91ee6409084a55616
Deepaw <danield.georgiev@gmail.com>
2021-03-08 14:26:52 +0200

    Adopt Me - Context - Context with Classes

--- javascript/react/adopt-me/src/Details.js
+++ javascript/react/adopt-me/src/Details.js
@@ -2,6 +2,7 @@ import React from "react";
 import pet from "@frontendmasters/pet";
 import Carousel from "./Carousel";
 import ErrorBoundary from "./ErrorBoundary";
+import ThemeContext from "./ThemeContext";
 
 class Details extends React.Component {
   state = { loading: true };
@@ -35,7 +36,18 @@ class Details extends React.Component {
         <div>
           <h1>{name}</h1>
           <h2>{`${animal} - ${breed} - ${location}`}</h2>
-          <button>Adopt {name}</button>
+          <ThemeContext.Consumer>
+            {/* Option 1 */}
+            {(themeHook) => (
+              <button style={{ backgroundColor: themeHook[0] }}>
+                Adopt {name}
+              </button>
+            )}
+            {/* Option 2 */}
+            {/* {([theme]) => (
+              <button style={{ backgroundColor: theme }}>Adopt {name}</button>
+            )} */}
+          </ThemeContext.Consumer>
           <p>{description}</p>
         </div>
       </div>
