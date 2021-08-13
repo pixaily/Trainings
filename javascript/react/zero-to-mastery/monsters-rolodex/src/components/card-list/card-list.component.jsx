c1cb191e330110301d0e19da00ae85fc3c3f9bf8
Deepaw <danield.georgiev@gmail.com>
2021-08-14 00:17:14 +0300

    React Zero To Mastery - React Basics -  Add card component

--- javascript/react/zero-to-mastery/monsters-rolodex/src/components/card-list/card-list.component.jsx
+++ javascript/react/zero-to-mastery/monsters-rolodex/src/components/card-list/card-list.component.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
+import { Card } from '../card/card.component';
 
 import './card-list.styles.css';
 
 export const CardList = (props) => {
-  console.log(props);
-
   return (
     <div className="card-list">
-      {props.monsters.map(monster => <h1 key={monster.id}>{monster.name}</h1>)}
+      {props.monsters.map(monster => <Card key={monster.id} monster={monster} />)}
     </div>
   )
 }
