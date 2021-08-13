96cb243b16b7f1303419ab50bc0af103bc9af20c
Deepaw <danield.georgiev@gmail.com>
2021-08-14 00:03:49 +0300

    React Zero To Mastery - React Basics - Migrate data from this.state to props

--- javascript/react/zero-to-mastery/monsters-rolodex/src/components/card-list/card-list.component.jsx
+++ javascript/react/zero-to-mastery/monsters-rolodex/src/components/card-list/card-list.component.jsx
@@ -7,7 +7,7 @@ export const CardList = (props) => {
 
   return (
     <div className="card-list">
-      {props.children}
+      {props.monsters.map(monster => <h1 key={monster.id}>{monster.name}</h1>)}
     </div>
   )
 }
