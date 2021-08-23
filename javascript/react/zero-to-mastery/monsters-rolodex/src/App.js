f89af0cf3039ea2949318a78913da8f138eac954
Deepaw <danield.georgiev@gmail.com>
2021-08-23 18:50:39 +0300

    React Zero To Mastery - React Basics -  Add final styles

--- javascript/react/zero-to-mastery/monsters-rolodex/src/App.js
+++ javascript/react/zero-to-mastery/monsters-rolodex/src/App.js
@@ -29,6 +29,7 @@ class App extends Component {
 
     return (
       <div className="App">
+        <h1>Monster Rolodex</h1>
         <SearchBox placeholder='Search monsters' handleChange={this.handleChange} />
         <CardList monsters={filterMonsters} />
       </div>
